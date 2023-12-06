018aaf0a8fb3878ec51992919123cef9af4eeb3e
Rivka Elgozy <rivkaelgozy@gamil.com>
2023-12-06 22:10:08 +0200

    parallel

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import cProfile
+
 from polynomial import Polynomial
 import sympy
 import pandas as pd
+import matplotlib.pyplot as plt
+import time
 import numpy as np
+import numba
+from numba import jit
 
 
 # learning polynomial library:
@@ -16,7 +22,6 @@ import numpy as np
 # q, remainder = divmod(p, Polynomial(1, 2))
 # print(str(remainder))
 # #2 --> (2x^2 + 4x + 2) mod (x + 2) = 2
-
 
 
 def get_prime():
@@ -34,21 +39,19 @@ def find_irreducible_poly(p):
     for a in range(1, p):
         for b in range(p):
             for c in range(p):
-                poly = sympy.Poly(a * x**2 + b * x + c,x)
+                poly = sympy.Poly(a * x ** 2 + b * x + c, x)
                 if not any(poly.subs(x, i) % p == 0 for i in range(p)):
                     return poly
     return None
 
 
-
 def find_polynomials2(p, coeffs_irreducible_poly):
-    x = sympy.Symbol('x')
     result = []
     for a in range(p):
         for b in range(p):
             # calculate w^p+w:
             w = Polynomial(a, b)
-            arrW = [a] + [0] * (p-1) + [b]
+            arrW = [a] + [0] * (p - 1) + [b]
             wPowP = Polynomial(arrW)
             remainderW = divmod(wPowP, Polynomial(coeffs_irreducible_poly))[1]
             remainderW = remainderW + w
@@ -57,7 +60,7 @@ def find_polynomials2(p, coeffs_irreducible_poly):
                 for d in range(p):
                     # calculate t^(p+1)
                     t = Polynomial(c, d)
-                    arrT = [c] + [0] * (p-1) + [d]
+                    arrT = [c] + [0] * (p - 1) + [d]
                     tPowP = Polynomial(arrT)
                     remainderT = divmod(tPowP, Polynomial(coeffs_irreducible_poly))[1]
                     remainderT = divmod(t * remainderT, Polynomial(coeffs_irreducible_poly))[1]
@@ -78,71 +81,150 @@ def printLinearLine(a, b, p, coeffs_irreducible_poly):
     result = []
     for z1 in range(p):
         for z2 in range(p):
-            z = Polynomial(z1,z2)
+            z = Polynomial(z1, z2)
             w = a[0]
             t = a[1]
             wTag = b[0]
             tTag = b[1]
-            #w:
+            # w:
             resW = w - wTag
             remainderW_mod_p = Polynomial([elem % p for elem in resW[:]][::-1])
             remainderW = divmod(z * remainderW_mod_p, Polynomial(coeffs_irreducible_poly))[1]
             res1 = remainderW + wTag
             res1_mod_p = Polynomial([elem % p for elem in res1[:]][::-1])
-            #t:
+            # t:
             resT = t - tTag
             remainderT_mod_p = Polynomial([elem % p for elem in resT[:]][::-1])
             remainderT = divmod(z * remainderT_mod_p, Polynomial(coeffs_irreducible_poly))[1]
             res2 = remainderT + tTag
             res2_mod_p = Polynomial([elem % p for elem in res2[:]][::-1])
             result.append((res1_mod_p, res2_mod_p))
-            #line = f"({wTag} + {z} * ({w} - {wTag}),({tTag} + {z} * ({t} - {tTag}))"
-            #print("The equation of the line is: F(z) =", (res1_mod_p, res2_mod_p))
-            #print(f"Fz1 = {res1_mod_p}, Fz2 = {res2_mod_p}")
     return result
 
-def findLines(polynomials1,p, coeffs_irreducible_poly):
-    result = []
-    df = pd.DataFrame(columns=['Line', 'Matches_counter'])
+
+
+
+def findLines(polynomials1, p, coeffs_irreducible_poly):
+    counter_values = []  # List to store counter values
+    hash_table = create_hash_table(polynomials1)
     for a in polynomials1:
-        for b in polynomials1:
-            #result += printLinearLine(a,b,p,coeffs_irreducible_poly)
-            result = printLinearLine(a,b,p,coeffs_irreducible_poly)
+        for b in [x for x in polynomials1 if x != a]:
+            result = printLinearLine(a, b, p, coeffs_irreducible_poly)
+            counter = matches_counter(result, hash_table)
+            counter_values.append(counter)
+    # result = printLinearLine((Polynomial(1), Polynomial(0)),(Polynomial(1), Polynomial(0)), p, coeffs_irreducible_poly)
+    # counter = matches_counter(result, hash_table)
+    # counter_values.append(counter)
 
-            counter = matchesCounter2(polynomials1,result)
+    # Create a histogram of integer counter values
+    unique_counters = list(set(counter_values))  # Get unique counter values
+    unique_counters.append(min(unique_counters) - 1)
+    unique_counters.append(max(unique_counters) + 1)
+    unique_counters.sort()  # Sort the unique counter values
 
-            # Create a new row as a dictionary
-            new_row = {'Line': f'(({a[0]},{a[1]}),({b[0]},{b[1]}))', 'Matches_counter': counter}
-            # Use append() to add the new row to the DataFrame
-            df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-            #print(df)
+    # Create a histogram with integer counter values
+    plt.hist(counter_values, bins=unique_counters, align='left', rwidth=0.8)
+    plt.xlabel('Counter')
+    plt.ylabel('Frequency')
+    plt.xticks(unique_counters)  # Set x-axis ticks to unique counters
+    plt.title('Counter Frequency')
+    plt.show()
 
-            #test for each a,b
-    #return result
-    pd.set_option('display.max_rows', None)
-    return df
 
-def matchesCounter2(polynomials1, result):
+def create_hash_table(polynomials):
+    hash_table = {}
+    for w,t in polynomials:
+        key = (f"{w}", f"{t}")
+        hash_table[key] = True
+
+    return hash_table
+
+
+def matches_counter(result, hash_table):
     count = 0
-    for w, t in result:#F_(a,b)z
-        for w2, t2 in polynomials1:
-            if w == w2 and t == t2:
+    for w,t in result:
+        try:
+            key = (f"{w}", f"{t}")
+            if hash_table[key]:
                 count += 1
-                break
+        except KeyError:
+            pass  # Key doesn't exist, continue without incrementing count
     return count
 
-    #return len(set(polynomials1) & set(result))
+import multiprocessing
 
-def matchesCounter(polynomials1, polynomials2):
-    count = 0
-    for w, t in polynomials2:#F_(a,b)z
-        for w2, t2 in polynomials1:
-            if w == w2 and t == t2:
-                count += 1
-                break
+def process_combination_wrapper(args):
+    a, b, p, coeffs_irreducible_poly, hash_table = args
+    result = printLinearLine(a, b, p, coeffs_irreducible_poly)
+    counter = matches_counter(result, hash_table)
+    return counter
 
-    print("Number of matching pairs:", count)
+def findLinesParallel(polynomials1, p, coeffs_irreducible_poly):
+    counter_values = []
+    hash_table = create_hash_table(polynomials1)
 
+    # Create a list of argument tuples for each combination of a and b
+    combinations = [(a, b, p, coeffs_irreducible_poly, hash_table) for a in polynomials1 for b in polynomials1 if b != a]
+    p = multiprocessing.Pool(multiprocessing.cpu_count())
+    results = p.map(process_combination_wrapper, combinations)
+
+    # Use multiprocessing to parallelize the work
+    # with multiprocessing.Pool() as pool:
+    #     results = pool.map(process_combination_wrapper, combinations)
+
+    # Combine the results from different processes
+    counter_values.extend(results)
+
+    # Rest of your code remains unchanged
+    unique_counters = list(set(counter_values))
+    unique_counters.append(min(unique_counters) - 1)
+    unique_counters.append(max(unique_counters) + 1)
+    unique_counters.sort()
+
+    plt.hist(counter_values, bins=unique_counters, align='left', rwidth=0.8)
+    plt.xlabel('Counter')
+    plt.ylabel('Frequency')
+    plt.xticks(unique_counters)
+    plt.title('Counter Frequency')
+    plt.show()
+
+    pd.set_option('display.max_rows', None)
+
+# from multiprocessing import Process, Queue, current_process
+# import queue
+# from multiprocessing import cpu_count
+#
+# def process_combination_wrapper(tasks_to_accomplish, tasks_that_are_done):
+#     while True:
+#         try:
+#             task = tasks_to_accomplish.get_nowait()
+#         except queue.Empty:
+#             break
+#         else:
+#             a, b, p, coeffs_irreducible_poly, hash_table = task
+#             result = printLinearLine(a, b, p, coeffs_irreducible_poly)
+#             counter = matches_counter(result, hash_table)
+#             tasks_that_are_done.put(counter)
+#
+# def findLinesParallel(tasks_to_accomplish, tasks_that_are_done, polynomials1, p, coeffs_irreducible_poly):
+#     number_of_processes = cpu_count()
+#     processes = []
+#     hash_table = create_hash_table(polynomials1)
+#
+#     # preparing tasks and starting parallel processing
+#     for a in polynomials1:
+#         for b in [x for x in polynomials1 if x != a]:
+#             tasks_to_accomplish.put((a, b, p, coeffs_irreducible_poly, hash_table))
+#
+#     # creating processes
+#     for _ in range(number_of_processes):
+#         p = Process(target=process_combination_wrapper, args=(tasks_to_accomplish, tasks_that_are_done))
+#         processes.append(p)
+#         p.start()
+#
+#     # joining processes
+#     for p in processes:
+#         p.join()
 
 
 def main():
@@ -152,13 +234,32 @@ def main():
     coeffs_irreducible_poly = irreducible_poly.all_coeffs()
     polynomials1 = find_polynomials2(p, coeffs_irreducible_poly)
     printPolyPoints(polynomials1)
-    ########################################################
-    polynomials2DF = findLines(polynomials1,p, coeffs_irreducible_poly)
-    print(polynomials2DF)
-    #print(polynomials2)
-    #matchesCounter(polynomials1,polynomials2)
+    #findLines(polynomials1, p, coeffs_irreducible_poly)
+    findLinesParallel(polynomials1, p, coeffs_irreducible_poly)
+    # tasks_to_accomplish = Queue()
+    # tasks_that_are_done = Queue()
+    #
+    # findLinesParallel(tasks_to_accomplish, tasks_that_are_done, polynomials1, p, coeffs_irreducible_poly)
+    # # print the output
+    # while not tasks_that_are_done.empty():
+    #     #print(tasks_that_are_done.get())
+    #     counter_values.append(tasks_that_are_done.get())
 
+    # # Create a histogram of integer counter values
+    # unique_counters = list(set(counter_values))  # Get unique counter values
+    # unique_counters.append(min(unique_counters) - 1)
+    # unique_counters.append(max(unique_counters) + 1)
+    # unique_counters.sort()  # Sort the unique counter values
+    #
+    # # Create a histogram with integer counter values
+    # plt.hist(counter_values, bins=unique_counters, align='left', rwidth=0.8)
+    # plt.xlabel('Counter')
+    # plt.ylabel('Frequency')
+    # plt.xticks(unique_counters)  # Set x-axis ticks to unique counters
+    # plt.title('Counter Frequency')
+    # plt.show()
 
 
 if __name__ == "__main__":
     main()
+    # cProfile.run('main()', sort='cumulative')
